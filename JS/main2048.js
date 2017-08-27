6013051f9b37d7fbbebcf278f85389725a505963
fengnianya <m18981924136@163.com>
2017-08-27 12:18:10 +0800

    can play with mobile
    可以在手机上玩了，滑动代替键盘。同时在index里做了缩放0.7。目的是手机端的显示

--- JS/main2048.js
+++ JS/main2048.js
@@ -44,43 +44,7 @@ function init(){
    $("#score").text(score);
 }
 
-//随机生成一个数2或4
-function generateOneNumber(){
-  //先看有无空格
-    if(nospace(board)){
-        return false;
-    }
 
-    //随机生成一个位置
-    var randx=parseInt(Math.floor(Math.random()*4));
-    var randy=parseInt(Math.floor(Math.random()*4));
-//看是不是空格,优化随机
-    var times=0;
-    while(times<50){
-        if(board[randx][randy]==0){
-            break;
-        }
-        //重复
-        var randx=parseInt(Math.floor(Math.random()*4));
-        var randy=parseInt(Math.floor(Math.random()*4));
-
-        times++;
-    }
-    if(times==50){
-        for(var i=0;i<4;i++){
-            for(var j=0;j<4;j++){
-                if(board[i][j]==0){
-                    randx=i;
-                    randy=j;
-                }
-            }
-        }
-    }
-    // 在格子上随机生成一个数字
-    var randNumber=Math.random()<0.65?2:4;
-    showNumberWithAnimation(randx,randy,randNumber);
-    board[randx][randy]=randNumber;
-}
 
 //键盘控制
 $(document).keydown(function (event){
